d7923948b001a43d528fe9eb93024bf1c22cd7e7
Veronny Jhony Morales Castillo <veronny393@hotmail.com>
2023-03-27 12:40:34 -0500

    permisos por grupos

--- filiacion/admin.py
+++ filiacion/admin.py
@@ -1,12 +1,13 @@
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
-
 from django.contrib import admin
+from django.contrib.auth.models import Permission
+
 from .models import Filiacion, Red, Microred, Establecimiento, Provincia, Distrito, Empleado, ImportaMarcador, MarcadorEmpleado, PapeletaHora, PapeletaDia
 
 
 #------------Red-------------------------------
-
+admin.site.register(Permission)
 
 # Register your models here.
 #------------Red---------------------------------
